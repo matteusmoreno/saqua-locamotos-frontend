25329e7a78e6b34cf27905014d2b5d712e4bdb0c
Matteus Moreno <matteus.moreno_cadmus@prestador.globo>
2026-03-31 11:53:43 -0300

    update api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://152.67.34.202:9292';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://saqualocamotos.qzz.io/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
